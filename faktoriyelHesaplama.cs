3bd688c2ac19fa6d0caa6ebb6d0550f4ce7c0b54
fyrelnell <fyrelnell@gmail.com>
2023-04-11 02:13:17 +0300

    Add

---
+++ faktoriyelHesaplama.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tekrar:
+            int rakam;
+            int rakam2;
+            decimal sonuc = 1;
+            int a = 0;
+
+            Console.Write("Bir Sayı Giriniz: ");
+            rakam = Convert.ToInt16(Console.ReadLine());
+
+            rakam2= rakam + a;
+            while (rakam > 1)
+            {
+                sonuc *= rakam;
+                rakam--;
+            }
+            Console.WriteLine("\n**********");
+            Console.WriteLine("{0} Sayısının Faktöriyeli: {1}\n",rakam2, sonuc );
+
+
+            Console.ReadKey();
+            Console.Clear();
+            goto tekrar;
+        }
+    }
+}
